4e974d137cdc81cdd10bcadccea71397cc73533f
phoeb <sengniphoebe.chung@my.jcu.edu.au>
2023-08-10 15:35:31 +0800

    practical 4

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,19 @@
+numbers = []
+for i in range(5):
+    numbers_list = int(input("Number: "))
+    numbers.append(numbers_list)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average number is {sum(numbers) / len(numbers)}")
+
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+name = input("Enter name: ")
+if name in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
---
+++ prac_04/quick_picks.py
@@ -0,0 +1,23 @@
+import random
+
+RANDOM_NUMBERS = 6
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 45
+
+quick_pick_number = int(input("How many quick picks? "))
+
+while quick_pick_number <= 0:
+    print("Quick pick number is greater than 0")
+    quick_pick_number = int(input("Enter number of quick picks: "))
+
+for i in range(quick_pick_number):
+    numbers = []
+    for j in range (RANDOM_NUMBERS):
+        number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        while number in numbers:
+            number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        numbers.append(number)
+    numbers.sort()
+    print(" ". join(f"{number:2}" for number in numbers))
+
+
