44748e20ed176790cde3812181b9d84087b8351f
phoeb <sengniphoebe.chung@my.jcu.edu.au>
2023-08-29 15:33:03 +0800

    Complete first part of More Guitars exercise

---
+++ prac_07/guitar.py
@@ -0,0 +1,27 @@
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        # return f"My guitar: {self.name}, first made in {self.year}"
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
+
+    def get_age(self):
+        current_year = 2022
+        age = current_year - self.year
+        return age
+
+    def is_vintage(self):
+        age = self.get_age()
+        return age >= 50
+
+    def __lt__(self, other):
+        return self.year < other.year
+
+
+
+
---
+++ prac_07/myguitars.py
@@ -0,0 +1,42 @@
+
+import csv
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Read file of guitar details, save as objects, display."""
+    guitars = []
+
+    # Open the file for reading
+    in_file = open('guitars.csv', 'r')
+    in_file.readline()  # Consume the header line
+
+    for line in in_file:
+        parts = line.strip().split(',')
+        language = Guitar(parts[0], parts[1], float(parts[2]))
+        guitars.append(language)
+
+    in_file.close()
+
+    # Sort guitars based on year
+    sorted_guitars = sorted(guitars, key=lambda x: x.year)
+
+    # Print sorted guitar details
+    for guitar in sorted_guitars:
+        print(guitar)
+
+# Call the main function
+main()
+
+
+def using_csv():
+    """Language file reader version using the csv module."""
+    # First, open the file for reading - note: specify newline
+    # to avoid quoted \n in strings being considered a new record
+    in_file = open('guitars.csv', 'r', newline='')
+    in_file.readline()
+    reader = csv.reader(in_file)  # use default dialect, Excel
+    for row in reader:
+        print(row)
+    in_file.close()
+
